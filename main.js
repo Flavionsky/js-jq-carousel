e16ec8b6516d1309b759cb2e6c27a6bf9fef74af
Flavio Muscente <flaviomuscente@libero.it>
2020-11-04 10:25:43 +0100

    add keyboard arrow comand

--- main.js
+++ main.js
@@ -1,34 +1,49 @@
 $(document).ready(function () {
 
+    $('.slider-wrapper > .prev').click(prevSlide);
+    $('.slider-wrapper > .next').click(nextSlide);
     var firstImgEl = $('.first');
     var firstCircleEl = $('.first');
     var lastImgEl = $('.last');
     var lastCircleEl = $('.last');
 
-    $('.slider-wrapper > .next').click(function (){
+    $(document).keyup(function(event){
+
+      switch(event.which){
+
+        case 37:
+        prevSlide();
+        break;
+        case 39:
+        nextSlide();
+        break;
+      }
+
+    })
+
+    function nextSlide(){
 
       var activeImageEl = $('img.active');
       var circleEl = $('i.active');
 
       if(activeImageEl.hasClass('last') && circleEl.hasClass('last')){
 
-          firstImgEl.addClass('active');
-          firstCircleEl.addClass('active');
-          activeImageEl.removeClass('active');
-          circleEl.removeClass('active');
+        firstImgEl.addClass('active');
+        firstCircleEl.addClass('active');
+        activeImageEl.removeClass('active');
+        circleEl.removeClass('active');
 
-        } else {
+      } else {
 
-          activeImageEl.removeClass('active');
-          circleEl.removeClass('active');
-          activeImageEl.next().addClass('active');
-          circleEl.next().addClass('active');
+        activeImageEl.removeClass('active');
+        circleEl.removeClass('active');
+        activeImageEl.next().addClass('active');
+        circleEl.next().addClass('active');
 
-        }
+      }
+    }
 
-    })
-
-    $('.slider-wrapper > .prev').click(function (){
+    function prevSlide(){
 
       var activeImageEl = $('img.active');
       var circleEl = $('i.active');
@@ -40,14 +55,14 @@ $(document).ready(function () {
         activeImageEl.removeClass('active');
         circleEl.removeClass('active');
 
-        } else{
+      } else{
 
-          activeImageEl.removeClass('active');
-          circleEl.removeClass('active');
-          circleEl.prev().addClass('active');
-          activeImageEl.prev().addClass('active');
+        activeImageEl.removeClass('active');
+        circleEl.removeClass('active');
+        circleEl.prev().addClass('active');
+        activeImageEl.prev().addClass('active');
 
-        }
+      }
+    }
 
-    })
 })
